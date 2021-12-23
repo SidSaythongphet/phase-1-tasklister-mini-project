07de0e4954e08b8ea749ddca89fa95da6ac63ebb
Sid Saythongphet <sid.saythongphet@gmail.com>
2021-12-23 16:16:09 -0500

    Completed main Deliverables

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    addTask(e.target['new-task-description'].value)
+    form.reset()
+  })
 });
+
+
+function addTask(task) {
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.textContent = 'X'
+  li.textContent = `${task} `
+  li.appendChild(btn)
+  document.querySelector('#tasks').appendChild(li)
+  btn.addEventListener('click', () => {
+    li.remove()
+  })
+}
